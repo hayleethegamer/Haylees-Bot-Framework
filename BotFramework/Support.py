ba77492844e167b878d958b52c4be4d60f0e9127
hayleethegamer <18407070+hayleethegamer@users.noreply.github.com>
2020-08-06 18:35:25 +0000

    Update Support.py

--- BotFramework/Support.py
+++ BotFramework/Support.py
@@ -1,4 +1,4 @@
- '''Copyright (C) 2020  Hayleethegamer
+'''Copyright (C) 2020  Hayleethegamer
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
